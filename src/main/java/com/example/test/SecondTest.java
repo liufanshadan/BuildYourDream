d2c8d0d25e5d95e3ee5a5ceb73d66f6fbe466abf
liufanshadan <1054547047@qq.com>
2021-12-07 17:32:43 +0800

    test

---
+++ src/main/java/com/example/test/SecondTest.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+    @Test
+    public void test1(){
+        System.out.println("操你妈的");
+        System.out.println("操你大爷");
+    }
+}
