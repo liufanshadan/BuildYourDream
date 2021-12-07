ed2e72c3f2f1a435e9b26892cf79257babf8c1f5
liufanshadan <1054547047@qq.com>
2021-12-07 20:33:08 +0800

    ok

---
+++ src/test/java/com/example/test/FirstTest.java
@@ -0,0 +1,12 @@
+package com.example.test;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void test1(){
+        System.out.println("买房");
+        System.out.println("买车");
+        System.out.println("买尊严");
+    }
+}
