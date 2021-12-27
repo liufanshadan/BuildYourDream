12ef1a082e24b0c9181d23c11f40513e052cfd6d
liufanshadan <1054547047@qq.com>
2021-12-27 19:32:46 +0800

    ok

---
+++ src/main/java/com/example/test/ThirdTest.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.testng.annotations.Test;
+
+public class ThirdTest {
+    @Test
+    public void test3(){
+        System.out.println("房子3");
+        System.out.println("车子3");
+        System.out.println("票子3");
+        System.out.println("happiness");
+    }
+}
